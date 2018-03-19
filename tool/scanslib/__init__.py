3538ec9f99bc3f7615453bc4c152b39a831f587a
Ash Wilson <ash.d.wilson@gmail.com>
2018-03-19 12:31:42 -0700

    chg: dev: Incrementing minor version to 0.17. !minor

--- tool/scanslib/__init__.py
+++ tool/scanslib/__init__.py
@@ -2,5 +2,5 @@ from get_scans import GetScans  # NOQA
 from outfile import Outfile  # NOQA
 from utility import Utility  # NOQA
 
-__version__ = "0.16"
+__version__ = "0.17"
 __author__ = "toolbox@cloudpassage.com"
